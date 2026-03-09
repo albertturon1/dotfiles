6820034006218dd59a87b70e502c8aa842836c03
Albert Turon <39496218+albertturon1@users.noreply.github.com>
2026-03-10 00:28:36 +0100

    feat: add git blame and change grep preview layout

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -425,6 +425,24 @@ require('lazy').setup({
   },
 
   {
+    'f-person/git-blame.nvim',
+    -- load the plugin at startup
+    event = 'VeryLazy',
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin will only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = true, -- if you want to enable the plugin
+      message_template = '<summary> • <author> • <date> • <<sha>>', -- template for the blame message, check the Message template section for more options
+      date_format = '%m.%d.%Y %H:%M:%S', -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
+
+  {
     'nvim-tree/nvim-tree.lua',
     version = '*',
     lazy = false,
@@ -543,6 +561,7 @@ require('lazy').setup({
         --
         defaults = {
           layout_strategy = 'flex',
+          sorting_strategy = 'ascending',
           layout_config = {
             horizontal = {
               prompt_position = 'top',
@@ -554,10 +573,11 @@ require('lazy').setup({
               prompt_position = 'top',
               height = { padding = 0 },
               width = { padding = 0 },
-              preview_height = 0.5,
+              preview_height = 0.55,
+              mirror = true,
             },
             flex = {
-              flip_coluns = 120,
+              flip_columns = 120,
             },
           },
           mappings = {
@@ -571,7 +591,17 @@ require('lazy').setup({
             },
           },
         },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            find_command = {
+              'rg',
+              '--files',
+              '--hidden',
+              '--glob',
+              '!.git/*',
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
